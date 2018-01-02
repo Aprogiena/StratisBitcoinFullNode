56adb74b09561f0d91f51726cb35634969421a83
Aprogiena <aprogiena@users.noreply.github.com>
2018-01-02 11:18:59 +0000

    enrich the queue with manual trigger option

--- src/Stratis.Bitcoin/Utilities/AsyncQueue.cs
+++ src/Stratis.Bitcoin/Utilities/AsyncQueue.cs
@@ -9,12 +9,18 @@ namespace Stratis.Bitcoin.Utilities
     /// Async queue is a simple thread-safe queue that asynchronously executes 
     /// a user-defined callback when a new item is added to the queue.
     /// <para>
-    /// The queue guarantees that the user-defined callback is executed only once at the time. 
-    /// If another item is added to the queue, the callback is called again after the current execution 
-    /// is finished.
+    /// Additionally, it allows manual trigger of a second user-defined callback.
     /// </para>
     /// </summary>
     /// <typeparam name="T">Type of items to be inserted in the queue.</typeparam>
+    /// <remarks>
+    /// The queue guarantees that the user-defined callbacks are executed only once at the time. 
+    /// If an item is added to the queue while the callback is being executed, the on-enqueue callback will be called 
+    /// after the current execution is finished. If a manual signal is triggered while the on-enqueue callback
+    /// is being executed, it on-signal callback will be executed after the current execution is finished.
+    /// If a manual signal is triggered while the on-signal callback is being executed, it will not be executed 
+    /// for the second time.
+    /// </remarks>
     public class AsyncQueue<T>: IDisposable
     {
         /// <summary>
@@ -24,14 +30,26 @@ namespace Stratis.Bitcoin.Utilities
         /// <param name="cancellationToken">Cancellation token that the callback method should use for its async operations to avoid blocking the queue during shutdown.</param>
         public delegate Task OnEnqueueAsync(T item, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Represents a callback method to be executed when <see cref="Signal"/> is called.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token that the callback method should use for its async operations to avoid blocking the queue during shutdown.</param>
+        public delegate Task OnSignalAsync(CancellationToken cancellationToken);
+
         /// <summary>Storage of items in the queue that are waiting to be consumed.</summary>
         private readonly ConcurrentQueue<T> items;
 
         /// <summary>Event that is triggered when at least one new item is waiting in the queue.</summary>
-        private readonly AsyncManualResetEvent signal;
+        private readonly AsyncManualResetEvent enqueuedSignal;
+
+        /// <summary>Event that is triggered when <see cref="Signal"/> is called.</summary>
+        private readonly AsyncManualResetEvent manualSignal;
 
         /// <summary>Callback routine to be called when a new item is added to the queue.</summary>
         private readonly OnEnqueueAsync onEnqueueAsync;
+
+        /// <summary>Callback routine to be called when <see cref="Signal"/> is called, or <c>null</c> if this callback is not used.</summary>
+        private readonly OnSignalAsync onSignalAsync;
 
         /// <summary>Consumer of the items in the queue which responsibility is to execute the user defined callback.</summary>
         private readonly Task consumerTask;
@@ -43,13 +61,17 @@ namespace Stratis.Bitcoin.Utilities
         /// Initializes the queue.
         /// </summary>
         /// <param name="onEnqueueAsync">Callback routine to be called when a new item is added to the queue.</param>
-        public AsyncQueue(OnEnqueueAsync onEnqueueAsync)
+        /// <param name="onSignalAsync">Callback routine to be called when <see cref="Signal"/> is called, or <c>null</c> if this callback is not used.</param>
+        public AsyncQueue(OnEnqueueAsync onEnqueueAsync, OnSignalAsync onSignalAsync = null)
         {
             Guard.NotNull(onEnqueueAsync, nameof(onEnqueueAsync));
 
             this.items = new ConcurrentQueue<T>();
-            this.signal = new AsyncManualResetEvent();
+            this.enqueuedSignal = new AsyncManualResetEvent();
+            this.manualSignal = new AsyncManualResetEvent();
+
             this.onEnqueueAsync = onEnqueueAsync;
+            this.onSignalAsync = onSignalAsync;
             this.cancellationTokenSource = new CancellationTokenSource();
             this.consumerTask = ConsumerAsync();
         }
@@ -61,7 +83,7 @@ namespace Stratis.Bitcoin.Utilities
         public void Enqueue(T item)
         {
             this.items.Enqueue(item);
-            this.signal.Set();
+            this.enqueuedSignal.Set();
         }
 
         /// <summary>
@@ -75,15 +97,26 @@ namespace Stratis.Bitcoin.Utilities
             {
                 try
                 {
-                    // Wait for an item to be enqueued.
-                    await this.signal.WaitAsync(cancellationToken).ConfigureAwait(false);
-                    this.signal.Reset();
+                    // Wait for an item to be enqueued or Signal to be called.
+                    Task task = await Task.WhenAny(this.enqueuedSignal.WaitAsync(cancellationToken), this.manualSignal.WaitAsync(cancellationToken)).ConfigureAwait(false);
 
-                    // Dequeue all items and execute the callback.
-                    T item;
-                    while (this.items.TryDequeue(out item) && !cancellationToken.IsCancellationRequested)
+                    if (this.manualSignal.IsSet)
                     {
-                        await this.onEnqueueAsync(item, cancellationToken).ConfigureAwait(false);
+                        // First execute the callback, then reset the event to ensure the signals sent meanwhile are ignored.
+                        await this.onSignalAsync(cancellationToken).ConfigureAwait(false);
+                        this.manualSignal.Reset();
+                    }
+
+                    if (this.enqueuedSignal.IsSet)
+                    {
+                        // First reset the event, then consume the queue to make sure all items are consumed from the queue.
+                        this.enqueuedSignal.Reset();
+
+                        T item;
+                        while (this.items.TryDequeue(out item) && !cancellationToken.IsCancellationRequested)
+                        {
+                            await this.onEnqueueAsync(item, cancellationToken).ConfigureAwait(false);
+                        }
                     }
                 }
                 catch (OperationCanceledException)
@@ -91,6 +124,14 @@ namespace Stratis.Bitcoin.Utilities
                     break;
                 }
             }
+        }
+
+        /// <summary>
+        /// Signals the event to ensure the on-signal callback is executed.
+        /// </summary>
+        public void Signal()
+        {
+            this.manualSignal.Set();
         }
 
         /// <inheritdoc/>
