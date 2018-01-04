5c9629136007a4423cb5c9fc039022ecaf0b2e71
Aprogiena <aprogiena@users.noreply.github.com>
2018-01-04 17:25:57 +0000

    separation of IMessageListener + removal of PollMessageListener

---
+++ src/Stratis.Bitcoin/P2P/Protocol/IMessageListener.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Stratis.Bitcoin.Utilities;
+
+namespace Stratis.Bitcoin.P2P.Protocol
+{
+    /// <summary>
+    /// Contract for a recipient part of a consumer logic that handles incoming messages.
+    /// </summary>
+    /// <typeparam name="T">Type of the messages that are being handled.</typeparam>
+    /// <seealso cref="MessageProducer{T}"/>
+    public interface IMessageListener<in T>
+    {
+        /// <summary>
+        /// Handles a newly received message.
+        /// </summary>
+        /// <param name="message">Message to handle.</param>
+        void PushMessage(T message);
+    }
+}
