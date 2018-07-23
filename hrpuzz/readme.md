b72b7abbdfdcc23d29638e24e47d722bf22a8d37
Aprogiena <Aprogiena@users.noreply.github.com>
2018-07-23 15:14:37 +0100

    Enjoy the puzzle!

---
+++ hrpuzz/readme.md
@@ -0,0 +1,37 @@
+Welcome to the Stratis puzzle and congratulations for going through the first stage! This already makes you special, 
+but you still have a long way to go if you want a prize.
+
+Your objective here is to solve 12 (or maybe more?) puzzles. You already solved the first one. 
+Solving a puzzle usually reveals a dictionary word. Together they form 12 word seed that you can use 
+to unlock the prize.
+
+If you solve all the puzzles you can claim the prize of at least 4000 STRATs unless, of course, 
+someone claims the prize before you do.
+
+The prize is stored in the following Stratis address: `SMnfE1HmgMpRWmtk3ABLsWDC1whoQYB7oB`
+
+You can use the [Stratis Blockchain Explorer](https://chainz.cryptoid.info/strat/address.dws?SMnfE1HmgMpRWmtk3ABLsWDC1whoQYB7oB.htm) to see if someone claimed the prize already.
+
+You don't need a permission or any kind of registration to start solving the puzzles and to claim the prize. 
+
+Additionally, we have 2 open job offers for a couple of participants who solve at least 10 puzzles and apply for the job.
+To apply for the job after solving at least 10 puzzles, contact `@aprogiena#0038` on [our Discord](https://discord.gg/9tDyfZs).
+We will interview you and ask you to demonstrate that you solved the puzzles yourself, 
+or at least that you are fully capable to do so. This means that just knowing 10 words from the seed won't get you the job :-).
+
+
+For community discussion about the puzzles and to get hints, visit `#puzzle` on [our Discord](https://discord.gg/9tDyfZs).
+We would highly appreciate your feedback, even if you don't want to share your progress with the community. 
+Please contact `@aprogiena#0038` on [our Discord](https://discord.gg/9tDyfZs), to provide your feedback and share with us 
+your progress. Also please contact us in case you find a bug in our puzzles.
+
+Are you ready? Do you think you have what it takes to become a Stratis developer? Or do you just want the money? All good!
+You can find the puzzles in the [words folder](https://github.com/Aprogiena/StratisBitcoinFullNode/tree/experiment/hrpuzz/hrpuzz/words). 
+To start solving the second puzzle, start with `market`.
+
+If a major bug is found in our puzzles, we will reward the one who reports it to us and we will publish a fix and explanation 
+on this page as soon as possible.
+
+Shortly after all prizes are claimed (the money and the 2 job offers), we will publish solutions to all puzzles on this page 
+to prove it was genuine. If the prize money is not claimed within 6 months, we will also publish the solutions to the puzzles 
+to prove it was genuine, but we will keep the money.
---
+++ src/Stratis.Bitcoin.Features.Consensus.Tests/PeerBanningTest.cs
@@ -0,0 +1,138 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NBitcoin;
+using Stratis.Bitcoin.Connection;
+using Stratis.Bitcoin.P2P.Peer;
+using Stratis.Bitcoin.P2P.Protocol.Behaviors;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.Consensus.Tests
+{
+    public class PeerBanningTest
+    {
+        public PeerBanningTest()
+        {
+            Block.BlockSignature = false;
+            Transaction.TimeStamp = false;
+        }
+
+        [Fact]
+        public async Task NodeIsSynced_PeerSendsABadBlockAndPeerDiconnected_ThePeerGetsBanned_Async()
+        {
+            string dataDir = Path.Combine("TestData", nameof(PeerBanningTest), nameof(this.NodeIsSynced_PeerSendsABadBlockAndPeerDiconnected_ThePeerGetsBanned_Async));
+            Directory.CreateDirectory(dataDir);
+
+            TestChainContext context = await TestChainFactory.CreateAsync(Network.RegTest, dataDir);
+            var peer = new IPEndPoint(IPAddress.Parse("1.2.3.4"), context.Network.DefaultPort);
+
+            context.MockReadOnlyNodesCollection.Setup(s => s.FindByEndpoint(It.IsAny<IPEndPoint>())).Returns((INetworkPeer)null);
+
+            var blocks = await TestChainFactory.MineBlocksAsync(context, 2, new Key().ScriptPubKey);
+            var block = blocks.First();
+            block.Header.HashPrevBlock = context.Chain.Tip.HashBlock;
+            await context.Consensus.AcceptBlockAsync(new BlockValidationContext { Block = block, Peer = peer });
+
+            Assert.True(context.PeerBanning.IsBanned(peer));
+        }
+
+        [Fact]
+        public async Task NodeIsSynced_PeerSendsABadBlockAndPeerIsConnected_ThePeerGetsBanned_Async()
+        {
+            string dataDir = Path.Combine("TestData", nameof(PeerBanningTest), nameof(this.NodeIsSynced_PeerSendsABadBlockAndPeerIsConnected_ThePeerGetsBanned_Async));
+            Directory.CreateDirectory(dataDir);
+
+            TestChainContext context = await TestChainFactory.CreateAsync(Network.RegTest, dataDir);
+            var peerEndPoint = new IPEndPoint(IPAddress.Parse("1.2.3.4"), context.Network.DefaultPort);
+
+            var connectionManagerBehavior = new ConnectionManagerBehavior(false, context.ConnectionManager, context.LoggerFactory);
+            var peer = new Mock<INetworkPeer>();
+            peer.Setup(p => p.Behavior<ConnectionManagerBehavior>()).Returns(connectionManagerBehavior);
+
+            context.MockReadOnlyNodesCollection.Setup(s => s.FindByEndpoint(It.IsAny<IPEndPoint>())).Returns(peer.Object);
+
+            var blocks = await TestChainFactory.MineBlocksAsync(context, 2, new Key().ScriptPubKey);
+            // create a new block that breaks consensus.
+            var block = blocks.First();
+            block.Header.HashPrevBlock = context.Chain.Tip.HashBlock;
+            await context.Consensus.AcceptBlockAsync(new BlockValidationContext { Block = block, Peer = peerEndPoint });
+            Assert.True(context.PeerBanning.IsBanned(peerEndPoint));
+        }
+
+        [Fact]
+        public async Task NodeIsSynced_PeerSendsABadBlockAndPeerIsWhitelisted_ThePeerIsNotBanned_Async()
+        {
+            string dataDir = Path.Combine("TestData", nameof(PeerBanningTest), nameof(this.NodeIsSynced_PeerSendsABadBlockAndPeerIsWhitelisted_ThePeerIsNotBanned_Async));
+            Directory.CreateDirectory(dataDir);
+
+            TestChainContext context = await TestChainFactory.CreateAsync(Network.RegTest, dataDir);
+            var peerEndPoint = new IPEndPoint(IPAddress.Parse("1.2.3.4"), context.Network.DefaultPort);
+
+            var connectionManagerBehavior = new ConnectionManagerBehavior(false, context.ConnectionManager, context.LoggerFactory) { Whitelisted = true };
+            var peer = new Mock<INetworkPeer>();
+            peer.Setup(p => p.Behavior<ConnectionManagerBehavior>()).Returns(connectionManagerBehavior);
+            context.MockReadOnlyNodesCollection.Setup(s => s.FindByEndpoint(It.IsAny<IPEndPoint>())).Returns(peer.Object);
+
+            var blocks = await TestChainFactory.MineBlocksAsync(context, 2, new Key().ScriptPubKey);
+            // create a new block that breaks consensus.
+            var block = blocks.First();
+            block.Header.HashPrevBlock = context.Chain.Tip.HashBlock;
+            await context.Consensus.AcceptBlockAsync(new BlockValidationContext { Block = block, Peer = peerEndPoint });
+
+            Assert.False(context.PeerBanning.IsBanned(peerEndPoint));
+        }
+        
+
+        [Fact]
+        public async Task NodeIsSynced_PeerSendsABadBlockAndErrorIsNotBanError_ThePeerIsNotBanned_Async()
+        {
+            string dataDir = Path.Combine("TestData", nameof(PeerBanningTest), nameof(this.NodeIsSynced_PeerSendsABadBlockAndErrorIsNotBanError_ThePeerIsNotBanned_Async));
+            Directory.CreateDirectory(dataDir);
+
+            TestChainContext context = await TestChainFactory.CreateAsync(Network.RegTest, dataDir);
+            var peerEndPoint = new IPEndPoint(IPAddress.Parse("1.2.3.4"), context.Network.DefaultPort);
+
+            var connectionManagerBehavior = new ConnectionManagerBehavior(false, context.ConnectionManager, context.LoggerFactory) { Whitelisted = true };
+            var peer = new Mock<INetworkPeer>();
+            peer.Setup(p => p.Behavior<ConnectionManagerBehavior>()).Returns(connectionManagerBehavior);
+            context.MockReadOnlyNodesCollection.Setup(s => s.FindByEndpoint(It.IsAny<IPEndPoint>())).Returns(peer.Object);
+
+            var blocks = await TestChainFactory.MineBlocksAsync(context, 2, new Key().ScriptPubKey);
+            // create a new block that breaks consensus.
+            var block = blocks.First();
+            block.Header.HashPrevBlock = context.Chain.Tip.HashBlock;
+            await context.Consensus.AcceptBlockAsync(new BlockValidationContext { Block = block, Peer = peerEndPoint, BanDurationSeconds = BlockValidationContext.BanDurationNoBan });
+
+            Assert.False(context.PeerBanning.IsBanned(peerEndPoint));
+        }
+
+        [Fact]
+        public async Task NodeIsSynced_PeerSendsABadBlockAndPeerIsBandAndBanIsExpired_ThePeerIsNotBanned_Async()
+        {
+            string dataDir = Path.Combine("TestData", nameof(PeerBanningTest), nameof(this.NodeIsSynced_PeerSendsABadBlockAndPeerIsBandAndBanIsExpired_ThePeerIsNotBanned_Async));
+            Directory.CreateDirectory(dataDir);
+
+            TestChainContext context = await TestChainFactory.CreateAsync(Network.RegTest, dataDir);
+            var peerEndPoint = new IPEndPoint(IPAddress.Parse("1.2.3.4"), context.Network.DefaultPort);
+
+            var connectionManagerBehavior = new ConnectionManagerBehavior(false, context.ConnectionManager, context.LoggerFactory) { Whitelisted = true };
+            var peer = new Mock<INetworkPeer>();
+            peer.Setup(p => p.Behavior<ConnectionManagerBehavior>()).Returns(connectionManagerBehavior);
+            context.MockReadOnlyNodesCollection.Setup(s => s.FindByEndpoint(It.IsAny<IPEndPoint>())).Returns(peer.Object);
+
+            var blocks = await TestChainFactory.MineBlocksAsync(context, 2, new Key().ScriptPubKey);
+            // create a new block that breaks consensus.
+            var block = blocks.First();
+            block.Header.HashPrevBlock = context.Chain.Tip.HashBlock;
+            await context.Consensus.AcceptBlockAsync(new BlockValidationContext { Block = block, Peer = peerEndPoint, BanDurationSeconds = 1 }); // ban for 1 second
+
+            // wait 1 sec for ban to expire.
+            Thread.Sleep(1000);
+
+            Assert.False(context.PeerBanning.IsBanned(peerEndPoint));
+        }
+    }
+}
